3d5702adc020c98df559a01ebb141ecc4f44112a
Tech-Avrach-Official <techavrach@gmail.com>
2025-05-16 22:27:27 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Home } from "lucide-react"
+import Navbar from "./component/Navbar"
+
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Home />
+    </div>
+  )
+}
+
+export default App
+
